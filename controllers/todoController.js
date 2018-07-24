74ca73a6ed77d868aa10afded766fa57df0bf23a
Porag <mofidulislamporag@gmail.com>
2018-07-24 19:43:06 +0600

    35 tutorial

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb://Porag:porag500600@ds147361.mlab.com:47361/todo22');
+
+//Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+	item: String 
+});
+
+var Todo = mongoose.model('TOdo',todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+	if (err) throw err;
+	console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'},{item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
